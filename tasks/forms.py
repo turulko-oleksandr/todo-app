3cb06b5c4c846322fa492ed2f0847f19793e963b
turulko-oleksandr <sashaturulko@i.ua>
2025-10-07 23:44:33 +0300

    feat(forms): add ModelForms for Task and Tag creation/editing

---
+++ tasks/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Task, Tag
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['content', 'deadline', 'tags']
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
